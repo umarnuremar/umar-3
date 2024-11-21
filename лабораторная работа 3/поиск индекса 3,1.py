2c0ffb6d5eec52bbe398fcc74bc3920b2b9cd1c8
umarnuremar <nuremar100@gmail.com>
2024-11-22 01:30:25 +0300

    Update поиск индекса 3,1.py

--- лабораторная работа 3/поиск индекса 3,1.py
+++ лабораторная работа 3/поиск индекса 3,1.py
@@ -1,16 +1,17 @@
 def find_item_index(items_list, item_to_find):
     try:
-        # Вернем индекс первого появления товара
+        # возвращаем индекс первого вхождения товара
         return items_list.index(item_to_find)
     except ValueError:
-        # Вернем None если не найдется товар
+        # если товар не найден, возвращаем None
         return None
+
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
 for find_item in ['банан', 'груша', 'персик']:
-    ind_item = find_item_index(items_list, find_item)  # вызов функции
-    if ind_item is not None:
-        print(f"Первое вхождение товара '{find_item}' имеет индекс {ind_item}.")
+    index_item = find_item_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
     else:
         print(f"Товар '{find_item}' не найден в списке.")
