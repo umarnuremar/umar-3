edbfd3c938c806a995447019482a1b4982650d5a
umarnuremar <nuremar100@gmail.com>
2024-11-22 01:33:09 +0300

    Update поиск общих участников 3,2.py

--- лабораторная работа 3/поиск общих участников 3,2.py
+++ лабораторная работа 3/поиск общих участников 3,2.py
@@ -1,19 +1,15 @@
-def find_common_participants(group1, group2, separator=','):
+def find_common_participants(group1, group2, delimiter=','):
 
-    #Разделим строки на списки участников
-    group1_participants = group1.split(separator)
-    group2_participants = group2.split(separator)
+    group1_participants = group1.split(delimiter)
+    group2_participants = group2.split(delimiter)
 
-    #Пересечение
     common_participants = list(set(group1_participants).intersection(group2_participants))
 
-    #Сортировка в алфавитном порядке
     common_participants.sort()
 
-    #Список общих участников
     return common_participants
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-participants_intersection = find_common_participants(participants_first_group, participants_second_group, separator='|')
+participants_intersection = find_common_participants(participants_first_group, participants_second_group, delimiter='|')
 print(participants_intersection)
